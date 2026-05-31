b512c71fd81b4f40c2e1c8972192288ec358b307
Shivam <shivamshinde00142@gmail.com>
2026-05-31 15:55:28 +0530

    Initial commit: Go movie streaming backend

---
+++ Server/MagicStreamMoviesServer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	controller "github.com/GavinLonDigital/MagicStreamMovies/Server/MagicStreamMoviesServer/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies!")
+	})
+	router.GET("/movies", controller.GetMovies())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("failed to start server", err)
+	}
+
+}
